aced6a53e2e4c5294ef45269c35a3b658ce5f2d9
tomino159 <92685486+tomino159@users.noreply.github.com>
2023-04-11 22:58:08 +0200

    Add files via upload

---
+++ tomino159-folder-creator.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import calendar
+
+month_names = list(calendar.month_name[1:])
+days = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10', 'Day 11', 'Day 12', 'Day 13', 'Day 14', 'Day 15', 'Day 16', 'Day 17', 'Day 18', 'Day 19', 'Day 20', 'Day 21', 'Day 22', 'Day 23', 'Day 24', 'Day 25', 'Day 26', 'Day 27', 'Day 28', 'Day 29', 'Day 30']
+
+for i, month in enumerate(month_names):
+    for day in days:
+        Path(f'2023/{i+1}.{month}/{day}').mkdir(parents=True, exist_ok=True)
